13a0754dcc1c697491d0b66a582d853b549e5347
ooa1769 <kimny86@gmail.com>
2018-06-13 11:38:46 +0900

    기본적인 Spring의 비동기 callback 방식 처리를 위한 ListenableFuturue방식

--- src/main/java/com/example/tobythread/TobyApplication.java
+++ src/main/java/com/example/tobythread/TobyApplication.java
@@ -11,20 +11,18 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Component;
-
-import java.util.concurrent.Future;
+import org.springframework.util.concurrent.ListenableFuture;
 
 @SpringBootApplication
 @Slf4j
 @EnableAsync
 public class TobyApplication {
 
-    // 1. 결과 -> 데이터베이스에 넣는다. db를 계속 바라보고 select에 결과가 있으면 결과값 가져온다.
-    // 2. HttpSession에 future객체를 저장하고 session에서 future를 꺼내 f.isDone이 true이면 결과값 가져온다.
+    //
     @Component
     public static class MyService {
         @Async
-        public Future<String> hello() throws InterruptedException {
+        public ListenableFuture<String> hello() throws InterruptedException {
             log.info("hello()");
             Thread.sleep(2000);
             return new AsyncResult<>("hello");
@@ -43,9 +41,9 @@ public class TobyApplication {
     ApplicationRunner run() {
         return args -> {
             log.info("run()");
-            Future<String> f = myService.hello();
-            log.info("exit : " + f.isDone());
-            log.info("result: " + f.get());
+            ListenableFuture<String> f = myService.hello();
+            f.addCallback(s -> System.out.println(s), e -> System.out.println(e.getMessage()));
+            log.info("exit");
         };
     }
 }
